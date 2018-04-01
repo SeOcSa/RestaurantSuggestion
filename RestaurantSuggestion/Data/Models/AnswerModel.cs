224ae92fc09921731523a4c5fa2b147d76aca8bb
sesa <sesa@DESKTOP-IA61TUT>
2018-04-01 20:03:31 +0300

    Adding datamodels for EntityFramework

--- RestaurantSuggestion/Data/Models/AnswerModel.cs
+++ RestaurantSuggestion/Data/Models/AnswerModel.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,12 @@ namespace RestaurantSuggestion.Data.Models
 {
     public class AnswerModel
     {
+        [Key]
+        public Guid AnswerId { get; set; }
+        public string AnswerText { get; set; }
+        [ForeignKey("QuestionModel")]
+        public Guid QuestionId { get; set; }
+        [ForeignKey("QuestionModel")]
+        public Guid NextQuestionId { get; set; }
     }
 }
--- RestaurantSuggestion/Data/Models/QuestionModel.cs
+++ RestaurantSuggestion/Data/Models/QuestionModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,8 @@ namespace RestaurantSuggestion.Data.Models
 {
     public class QuestionModel
     {
+        [Key]
+        public Guid QuestionId { get; set; }
+        public string QuestionText { get; set; }
     }
 }
